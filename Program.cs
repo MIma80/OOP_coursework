c1f18b4acfab500829c6f64bafac5e108c3bd42c
Mima80 <mimi451276@gmail.com>
2022-10-31 17:29:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PL_coursework;
+using DAL_coursework;
+using BLL_coursework;
+using System.IO;
+using DataProviderContract;
+using JSONProvider;
+
+namespace OOP_coursework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+           IDataContext<Wallet> dataContextCurrentWallet_json = new DataContext<Wallet>("DataBase.json");
+           dataContextCurrentWallet_json.DataProvider = new JsonDataProvider<Wallet>();
+           WalletService walletService = new WalletService(dataContextCurrentWallet_json);
+
+            Menu.Start(walletService);
+        }
+    }
+}
